a92a02f7670c0837670c465abaeef11b78df21dc
Fanzen123 <amir.boussadia@gmail.com>
2022-03-13 22:15:20 +0100

    PS-01 initial commit

---
+++ src/main/java/fr/lecko/api/SaveMicrosoftGraph.java
@@ -0,0 +1,10 @@
+package fr.lecko.api;
+
+import org.springframework.boot.SpringApplication;
+
+public class SaveMicrosoftGraph
+{
+    public static void main(String[] args) {
+        SpringApplication.run(SaveMicrosoftGraph.class, args);
+    }
+}
